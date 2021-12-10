e7d733a75ca53e3c35e5f627bc8e161081c46682
Emmanuel-Develops <itakpeemma@gmail.com>
2021-12-10 16:24:34 +0100

    implemented dashboard search feature

--- src/components/DashboardHeading.jsx
+++ src/components/DashboardHeading.jsx
@@ -1,10 +1,10 @@
 import SearchBar from "./SearchBar"
 
-const DashboardHeading = ({heading}) => {
+const DashboardHeading = ({heading, searchTerm, update}) => {
     return (
         <div className="flex justify-between items-start my-10">
           <h1 className="text-xl md:text-3xl text-blue-400 font-bold leading-relaxed tracking-wide">{heading}</h1>
-          <SearchBar />
+          <SearchBar query={searchTerm} setQuery={update} />
         </div>
     )
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import ProjectCard from './ProjectCard'
 import DashboardHeading from './DashboardHeading'
 import DashboardContext from '../state/projects-context'
@@ -6,11 +6,37 @@ import DashboardContext from '../state/projects-context'
 function Grid() {
 
     const context = useContext(DashboardContext)
-    const projects = context.state
+    // const projects = context.state
+    const initialState = context.state
+    const [projects, setProjects] = useState(initialState)
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const updateSearchTerm = (search) => {
+        setSearchTerm(search)
+    }
+  
+
+    useEffect(() => {
+
+        const updateProjects = (searchTerm) => {
+            const results = initialState.filter((project) => {
+                return project.name.toLowerCase().includes(searchTerm.toLowerCase())
+            })
+
+            setProjects(results)
+        }
+
+        setTimeout(() => {
+            updateProjects(searchTerm)
+        }, 500);
+
+        return clearTimeout()
+    }, [searchTerm])
+
 
     return (
         <>
-            <DashboardHeading heading={"Available Projects"} />
+            <DashboardHeading heading={"Available Projects"} searchTerm={searchTerm} update={updateSearchTerm} />
             {projects ? 
             <section className="grid my-5 grid-cols-1 gap-4 lg:gap-10 lg:grid-cols-2">
                 {projects.map((project) => 
